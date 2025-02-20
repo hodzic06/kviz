ff8f9a6206db9ddf8e0d928628bacd73c9a5d84f
Amar <amar.91hodzi@gmail.com>
2025-02-20 12:10:18 +0100

    starting js

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+const user = {
+    id: "1",
+    name: "Bob",
+}
+
+const stringifyUser = JSON.stringify(user);
+localStorage.setItem("user", stringifyUser);
+
+const userformStorage = JSON.parse(localStorage.getItem("user"));
+console.log(userformStorage)
+*/
